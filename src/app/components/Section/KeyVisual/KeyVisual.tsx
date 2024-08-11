d2ef58fef0f6d75bdc89f8cd1bb8fe99941a1502
Fuya Yamada <fuyamiwa713@gmail.com>
2024-08-11 13:51:54 +0900

    fix autoplay

--- src/app/components/Section/KeyVisual/KeyVisual.tsx
+++ src/app/components/Section/KeyVisual/KeyVisual.tsx
@@ -1,22 +1,31 @@
+"use client";
 import styles from "./KeyVisual.module.css";
 import nextConfig from "../../../../../next.config.mjs";
+import { useEffect, useRef } from "react";
 
 export default function KeyVisual() {
   const BASE_PATH = nextConfig.basePath || "";
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.play();
+    }
+  }, [videoRef]);
 
   return (
     <main className="main">
       <div className={styles.videoContainer}>
         <video
+          ref={videoRef}
           className={styles.video}
+          src={`${BASE_PATH}/demo.mp4`}
           width="600"
           loop
           autoPlay
           muted
           playsInline
-        >
-          <source src={`${BASE_PATH}/demo.mp4`} type="video/mp4" />
-        </video>
+        />
         <div className={styles.overlay}>
           <h2 className={styles.title}>FuyanTech</h2>
           <div className={styles.nameContainer}>
