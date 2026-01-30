6c4557fd87bc25c165425deada4aba5b4f304faf
Fuya Yamada <fuyamiwa713@gmail.com>
2026-01-31 00:49:27 +0900

    update

--- src/app/components/Section/ProductsList/ProductsList.tsx
+++ src/app/components/Section/ProductsList/ProductsList.tsx
@@ -3,26 +3,38 @@ import SectionHeader from "../../SectionHeader/SectionHeader";
 import styles from "./ProductsList.module.css";
 import nextConfig from "../../../../../next.config.mjs";
 
-const appItems: { [key: number]: { title: string; src: string; link: string } } = {
+const appItems: {
+  [key: number]: { title: string; src: string; link: string };
+} = {
   1: {
-    title: "アブローラーカウント",
-    src: "abs",
-    link: "https://apps.apple.com/jp/app/id1587434824",
+    title: "Vegitaberu",
+    src: "1_vegitaberu",
+    link: "https://apps.apple.com/jp/app/id6749199964",
   },
   2: {
-    title: "DreamTracker",
-    src: "dt",
-    link: "https://apps.apple.com/jp/app/dreamtracker/id6479675478",
+    title: "Yametoku",
+    src: "2_yametoku",
+    link: "https://apps.apple.com/jp/app/id6749310247",
   },
   3: {
-    title: "日焼け止めタイマー",
-    src: "sun",
-    link: "https://apps.apple.com/jp/app/id6461166544",
+    title: "Maisore",
+    src: "3_maisore",
+    link: "https://apps.apple.com/jp/app/id6751938161",
   },
   4: {
-    title: "ポートフォリオ",
-    src: "portfolio",
-    link: "/",
+    title: "Misel",
+    src: "4_misel",
+    link: "https://apps.apple.com/jp/app/id6752554103",
+  },
+  5: {
+    title: "Mirucode",
+    src: "5_mirucode",
+    link: "https://apps.apple.com/jp/app/id6756328501",
+  },
+  6: {
+    title: "Miradance",
+    src: "6_miradance",
+    link: "",
   },
 };
 
@@ -38,15 +50,33 @@ export default function ProductsList() {
         {cells.map((num) => {
           const app = appItems[num];
           if (app) {
-            return (
-              <Link href={app.link} key={num} className={`${styles.cell} ${styles.hasApp}`}>
-                <img
-                  src={`${BASE_PATH}/pd_${app.src}.png`}
-                  alt={app.title}
-                  className={styles.image}
-                />
-              </Link>
-            );
+            if (app.link) {
+              return (
+                <Link
+                  href={app.link}
+                  key={num}
+                  className={`${styles.cell} ${styles.hasApp}`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img
+                    src={`${BASE_PATH}/${app.src}.png`}
+                    alt={app.title}
+                    className={styles.image}
+                  />
+                </Link>
+              );
+            } else {
+              return (
+                <div key={num} className={`${styles.cell} ${styles.hasApp}`}>
+                  <img
+                    src={`${BASE_PATH}/${app.src}.png`}
+                    alt={app.title}
+                    className={styles.image}
+                  />
+                </div>
+              );
+            }
           }
           return (
             <div key={num} className={styles.cell}>
